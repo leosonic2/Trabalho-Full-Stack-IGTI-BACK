5ea2f7b78a67dd3a9d6b4f593797872abbef500d
Leandro Martins <leosonic2@gmail.com>
2020-07-12 19:26:13 -0300

    correcao no endereco

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://frontend-trabalho-mongo-8081.herokuapp.com/',
+    origin: 'https://frontend-trabalho-mongo-8081.herokuapp.com',
   })
 );
 
